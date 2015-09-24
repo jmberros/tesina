babf3aaf44402a0bc31aefb792a08d118c6144a7
Juan Manuel Berros <juanmaberros@gmail.com>
2015-09-24 18:13:18 -0300

    fixed centromers and new scatter of frequecies

--- chromosomes_with_SNPs_plot.py
+++ chromosomes_with_SNPs_plot.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def chromosomes_with_SNPs_plot(genome, snp_groups_dict):
@@ -6,25 +7,29 @@ def chromosomes_with_SNPs_plot(genome, snp_groups_dict):
     {'label': {'df': snp_dataframe, 'color': 'k', marker: 'x'}},
     where the dataframe has fields named chr and position"""
 
-    # Chromosomes
-    ax = genome.length.plot(kind="barh", width=0.50, figsize=(16, 20),
-                            edgecolor="lightgray", facecolor="snow",
-                            linewidth=3, zorder=0, label="Cromosomas")
+    ax = plt.subplot(111)
 
-    # Centromeres
-    ax.scatter(x=genome.centromere_start.values, y=genome.index.values,
-               zorder=1, marker='|', linewidth=2,
-               s=600, label='Centromere start', color='silver')
-    ax.scatter(x=genome.centromere_end.values, y=genome.index.values,
-               zorder=1, marker='|', linewidth=2,
-               s=600, label='Centromere end', color='silver')
+    # The chromosomes with their centromeres are kind of a hacky drawing.
+    # It's just three overlapping bars, and the middle one gets to be the
+    # centromere because only its tip is seen.
+
+    # p arms of chromosomes
+    genome.centromere_start.plot(ax=ax, kind="barh", facecolor="snow",
+                                 zorder=-1, label="", linewidth=0)
+    # centromeres
+    genome.centromere_end.plot(ax=ax, kind="barh", facecolor="silver",
+                               zorder=-2, label="Centrómeros", linewidth=0)
+
+    # q arms of chromosomes
+    genome.length.plot(ax=ax, kind="barh", width=0.50, figsize=(16, 20),
+                       facecolor="snow", zorder=-3, label="", linewidth=0)
 
     # SNPs
     for label, data in snp_groups_dict.items():
         ax.scatter(y=data['df'].chr.values - 1, x=data['df'].position.values,
-                   zorder=1, marker=data.get('marker', '|'), linewidth=2,
-                   s=data.get('s', 600), label=label,
-                   color=data.get('color', 'k'))
+                   zorder=1, marker=data.get('marker'), linewidth=2,
+                   s=data.get('s'), label=label,
+                   color=data.get('color'))
 
     max_len = genome.length.max()
     ax.set_xlim([0, max_len * 1.05])
