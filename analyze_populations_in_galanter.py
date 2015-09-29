32b508e19636116b7029d3f6f48ec7a2fefdb700
Juan Manuel Berros <juanmaberros@gmail.com>
2015-09-29 18:47:46 -0300

    boxplot to replace bar plot of frequencies by population

--- analyze_populations_in_galanter.py
+++ analyze_populations_in_galanter.py
@@ -1,15 +1,21 @@
+import matplotlib.pyplot as plt
 from helpers.data_munging_functions import annotate_bars
 
 
-def plot_mean_freqs(df, populations_to_plot, title=""):
+def boxplot_freqs_by_populations(df, populations_to_plot, title=""):
     [df.drop(field, axis=1, inplace=True)
      for field in df if field not in populations_to_plot]
 
-    ax = df.mean().plot(kind='bar', color='cornflowerBlue', figsize=(18, 6),
-                        width=0.75)
-    ax.set_ylim(0, 1)
+    # ax = df.mean().plot(kind='bar', color='cornflowerBlue', figsize=(18, 6),
+                        # width=0.75)
+
+    df.boxplot(rot=90, showmeans=True)
+    ax = plt.subplot(111)
+    ax.figure.set_figheight=6
+    ax.figure.set_figwidth=18
+    ax.set_ylim([0, 1])
     ax.set_title(title)
     ax.axhline(0.5, color='k', linestyle='--')
-    annotate_bars(ax, decimals=2, fontsize=10)
+    # annotate_bars(ax, decimals=2, fontsize=10)
 
     return ax
