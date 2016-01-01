883510611cd0d239bba663b4a7c670ecca8f76e7
Juan Manuel Berros <juanmaberros@gmail.com>
2016-01-01 18:40:00 -0300

    FTP walk instead of recursively called function

--- ftp_download_HGDP.py
+++ ftp_download_HGDP.py
@@ -1,77 +1,83 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#  import argparse
-import ftplib
+import ftputil
 import os
 
 from datetime import datetime
 from pprint import pprint
+from humanize import naturalsize
+from collections import OrderedDict
 
 
 def log(text, pprint_on=False):
-    if pprint_on and len(text) < 10:
-        pprint(text)
-        return
-
     if type(text) != str:
-        text = ", ".join(text)
+        if pprint_on and len(text) < 10:
+            print()
+            pprint(text)
+            print()
+            return
 
-    print(timestamp() + text)
+    print("[{}] {}".format(timestamp(), str(text)))
 
 
 def timestamp():
-    return "[{}] ".format(datetime.now().strftime("%H:%M"))
+    return datetime.now().strftime("%H:%M")
 
+def ftp_walk_and_download(domain, remotedir, user, passwd, basedir, localdir=None):
+    original_dir = os.getcwd()
+    log("Change to download directory: '{}'".format(basedir))
+    os.chdir(basedir)
 
-def ftp_download_all(target_dir, domain, user, passwd, dldir):
-    original_wd = os.getcwd()
-    log("FTP connecting to {}".format(domain))
-    ftp = ftplib.FTP(domain)
-    log("Login with credentials: '{}', '{}'".format(user, passwd))
-    ftp.login(user=user, passwd=passwd)
-    log("Change remote dir to: '{}'".format(dldir))
-    ftp.cwd(dldir)
-    all_files = ftp.nlst()
-    log("{} files found:".format(len(all_files)))
-    log(all_files, pprint_on=True)
-    target_dir and os.chdir(target_dir)
+    if os.path.isdir(localdir):
+        os.rename(localdir, remotedir)  # I'll rename back after the process
 
-    for f in all_files:
-        if os.path.isfile(f):
-            log("'{}': Existent file, skip download.".format(f))
-            continue
-        log("'{}': Downloading...".format(f))
-        ftp.retrbinary("RETR " + f, open(f, "wb").write)
+    log("Login to '{}' as: '{}', '{}'".format(domain, user, passwd))
+    with ftputil.FTPHost(domain, user, passwd) as host:
+        for root, dirs, files in host.walk(remotedir):
+            os.makedirs(root, exist_ok=True)
 
-    log("Check '{}'".format(os.getcwd()))
+            log("Walking down remote dir '{}'".format(root))
+            log("{} files found remotely.".format(len(files)))
+            for i, fpath in enumerate([os.path.join(root, fn) for fn in files]):
+                file_exists_locally = os.path.isfile(fpath)
+
+                filetag = "({}/{}) '{}'".format(i+1, len(files), fpath)
+                if file_exists_locally:
+                    if os.path.getsize(fpath) == host.path.getsize(fpath):
+                        log("{}: File exists, skip download.".format(filetag))
+                        continue
+
+                remote_filesize = naturalsize(host.path.getsize(fpath))
+                log("{}: Downloading {}".format(filetag, remote_filesize))
+                host.download(fpath, fpath)
+
+            if len(dirs) > 0:
+                log("Continue with dirs: {}".format(str(dirs)))
+                for dirname in dirs:
+                    os.makedirs(dirname, exist_ok=True)
+
+            print()
+
+    log("Done. Renaming '{}' to '{}'.".format(remotedir, localdir))
+    os.rename(remotedir, localdir)
+    os.chdir(original_dir)
     print()
-    ftp.quit()
-    os.chdir(original_wd)
 
 
 if __name__ == "__main__":
-    #  parser = argparse.ArgumentParser()
-    #  parser.add_argument("target_dir")
-    #  args = parser.parse_args()
+    dataset_dirnames = OrderedDict([
+        ("hgdp_v3", "dataset_1_HGDP-CEPH_v3"),
+        ("hgdp_supp1", "dataset_2_supp1_Stanford"),
+        ("hgdp_supp2", "dataset_3_supp2_UMichigan"),
+        ("hgdp_supp3", "dataset_4_supp3_MPlank"),
+        ("hgdp_supp15", "dataset_15_supp15_UCLA"),
+        ("hgdp_supp10", "dataset_11_supp10_Harvard"),
+    ])
 
-    ftp_download_all(target_dir="../HGDP_data/dataset_1_HGDP-CEPH_v3/",
-                     domain="ftp.cephb.fr",
-                     user="anonymous", passwd="anonymous",
-                     dldir="hgdp_v3/")
+    BASEDIR = "/home/juan/tesina/HGDP_data"
 
-    ftp_download_all(target_dir="../HGDP_data/dataset_2_supp1_Stanford/",
-                     domain="ftp.cephb.fr",
-                     user="anonymous", passwd="anonymous",
-                     dldir="hgdp_supp1/")
-
-    ftp_download_all(target_dir="../HGDP_data/dataset_3_supp2_UMichigan/",
-                     domain="ftp.cephb.fr",
-                     user="anonymous", passwd="anonymous",
-                     dldir="hgdp_supp2/GENO/")
-
-    ftp_download_all(target_dir="../HGDP_data/dataset_4_supp3_MPlank/",
-                     domain="ftp.cephb.fr",
-                     user="anonymous", passwd="anonymous",
-                     dldir="hgdp_supp3/")
-
+    for remotedir, localdir in dataset_dirnames.items():
+        ftp_walk_and_download(domain="ftp.cephb.fr", basedir=BASEDIR,
+                              remotedir=remotedir, localdir=localdir,
+                              user="anonymous", passwd="anonymous")
