68a47cbfe937008686254d85dc00b3978dddf9a8
Juan Manuel Berros <juanmaberros@gmail.com>
2016-01-03 17:49:41 -0300

    process CSV line in bash

--- thin_N_SNPs_from_chromosome_Q.sh
+++ thin_N_SNPs_from_chromosome_Q.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-CHR=$1
-COUNT=$2
-
 # Hardcoded stuff:
 BPSPACE=250000
 BASEDIR="/home/juan/tesina/1000Genomes_data"
-
-if [ -z $1 ] || [ -z $2 ]; then
-    echo ""
-    echo "Please pass two positional arguments:"
-    echo "$0 <chromosome_number> <number_of_SNPs_to_keep>"
-    echo ""
-    exit
-fi
-
+COUNTFILE="/home/juan/tesina/notebook/data/chr_SNP_count_in_galanter"
 OUTDIR="thinned-chromosomes"
-OUTFILE="thinned.chr${CHR}.total${COUNT}snps.every${BPSPACE}"
 
 mkdir -p "${BASEDIR}/${OUTDIR}"
 
-plink --vcf ${BASEDIR}/ALL.chr${CHR}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --snps-only --thin-count ${COUNT} --bp-space ${BPSPACE} --recode A-transpose --out ${BASEDIR}/${OUTDIR}/${OUTFILE}
+for CHR_COUNT in `cat "${COUNTFILE}"`; do
+    CHR_COUNT=(${CHR_COUNT//,/ })
+    CHR=${CHR_COUNT[0]}
+    COUNT=${CHR_COUNT[1]}
+
+    OUTFILE="thinned.chr${CHR}.total${COUNT}snps.every${BPSPACE}"
+
+    plink --vcf ${BASEDIR}/ALL.chr${CHR}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --snps-only --thin-count ${COUNT} --bp-space ${BPSPACE} --make-bed --out ${BASEDIR}/${OUTDIR}/${OUTFILE}
+
+done
 
 # Keep the 2nd field (dbsnp ID) and then from the 8th until the end (genotypes)
-cut ${BASEDIR}/${OUTDIR}/${OUTFILE}.traw -f 2,8-
-
+# cut ${BASEDIR}/${OUTDIR}/${OUTFILE}.traw -f 2,8-
