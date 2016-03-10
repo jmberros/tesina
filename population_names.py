5730ffd947e0c0ac2b6de936bbd86b95ea577072
Juan Manuel Berros <juanmaberros@gmail.com>
2016-03-09 22:18:36 -0300

    population_names minor fix

--- population_names.py
+++ population_names.py
@@ -6,11 +6,7 @@ def _get_1000g_pop_names_from_url():
     url = "http://www.1000genomes.org/category/" + \
           "frequently-asked-questions/population"
 
-    df = pd.read_html(url)[0]  # First table in the page
-    df.columns = df.iloc[0]
-    df = df.iloc[1:, :3].set_index('Population Code')
-
-    return df
+    return pd.read_html(url)[0]  # First table in the page
 
 
 def create_population_names_df(dumpfile):
@@ -18,6 +14,11 @@ def create_population_names_df(dumpfile):
         return pd.read_csv(dumpfile, index_col='Population Code')
 
     df = _get_1000g_pop_names_from_url()
-    df.to_csv(dumpfile)
 
+    # Complete truncated CEU description
+    ceu_description = 'Utah Residents (CEPH) with Northern ' + \
+                      'and Western European Ancestry'
+    df = df.set_value('CEU', 'Population Description', ceu_description)
+
+    df.to_csv(dumpfile)
     return df
