c60217f1357e6da8114a563333aca78359e76a19
Juan Manuel Berros <juanmaberros@gmail.com>
2016-03-10 18:12:10 -0300

    Before splitting the notebook

--- population_names.py
+++ population_names.py
@@ -6,19 +6,14 @@ def _get_1000g_pop_names_from_url():
     url = "http://www.1000genomes.org/category/" + \
           "frequently-asked-questions/population"
 
-    return pd.read_html(url)[0]  # First table in the page
+    return pd.read_html(url)[0]  # First table in the page:
 
 
 def create_population_names_df(dumpfile):
     if isfile(dumpfile):
         return pd.read_csv(dumpfile, index_col='Population Code')
 
-    df = _get_1000g_pop_names_from_url()
-
-    # Complete truncated CEU description
-    ceu_description = 'Utah Residents (CEPH) with Northern ' + \
-                      'and Western European Ancestry'
-    df = df.set_value('CEU', 'Population Description', ceu_description)
-
+    df = _get_1000g_pop_names_from_url().set_index("Population Code")
     df.to_csv(dumpfile)
+
     return df
